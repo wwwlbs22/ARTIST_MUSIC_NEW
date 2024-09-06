fe167745881d2d90266eb08d6aa04b12aa71832e
⚠️ ✘ 𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏 ✘ ⚠️ 🚩 <154514127+wwwlbs22@users.noreply.github.com>
2024-09-06 09:32:24 +0530

    Update start.py

--- Spotify_Music/utils/inline/start.py
+++ Spotify_Music/utils/inline/start.py
@@ -27,7 +27,7 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(text=" ʜᴇʟᴘ ", callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=" ᴅᴇᴠ ", url=f"https://t.me/ll_BAD_MUNDA_ll"),
+            InlineKeyboardButton(text=" 𝐎𝐖𝐍𝐄𝐑 ", url=f"https://t.me/ARTIST_FEELINGS"),
             ],
                     [
                     InlineKeyboardButton(text=" ᴀᴅᴅ ᴍᴇ ᴛᴏ ɢʀᴏᴜᴘ ", url=f"https://t.me/{app.username}?startgroup=true",
